c6091f1aa43e78c22839571d63944ceba07d7471
Marcin Kos <marcin.kos.krk@gmail.com>
2022-04-13 22:05:20 +0200

    callback hell

--- src/lib/callback.ts
+++ src/lib/callback.ts
@@ -1,8 +1,28 @@
-import { Callback } from "./interfaces";
+import { Callback, CallbackError, Hero } from "./interfaces";
+import axios, { AxiosError, AxiosResponse } from 'axios';
+import { apiUrl, parseList } from "./config";
+
+function getHeroCallback (
+    email: string,
+    callback: Callback<Hero>,
+    callbackError?: CallbackError 
+) {
+    axios
+        .get<Hero[]>(`${apiUrl}/heroes/email=${email}`)
+        .then((response: AxiosResponse<Hero[]>) => {
+            const data = parseList<Hero>(response);
+            const hero = data[0];
+            callback(hero);
+        })
+        .catch((error: AxiosError) => {
+            callbackError('We are unable to fetch Hero');
+        })
+}
 
 const getHeroTreeCallback = function(
     email: string,
-    callback: Callback<Hero>
+    callback: Callback<Hero>,
+    callbackError?: CallbackError
 ) {
-
-}
+    getHeroCallback(email, () => {}, (error: string) => {callbackError(error)})
+};
